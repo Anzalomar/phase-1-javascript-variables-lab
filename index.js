589499c3bb16f0cb41d6decf221e3b099e6c7afb
anzalomar <oanisa143@gmail.com>
2024-06-21 15:52:47 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file!
+// 1) is set as Scuber
+// 2) is defined as a const
+const companyName = 'Scuber';
+
+// 3) is declared as equal to Chelsea
+// 4) is defined using let
+let mostProfitableNeighborhood = 'Chelsea';
+
+// 5) is declared as equal to Susan Smith
+// 6) is defined using let
+let companyCeo = 'Susan Smith';
